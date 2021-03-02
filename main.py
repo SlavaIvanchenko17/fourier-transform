040789e14e8663fbbc3a9a5c4f474d252115e74f
SlavaIvanchenko17 <torstein.viking.2020@gmail.com>
2021-03-02 12:58:52 +0200

    add task

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import time as t
 
 def gen_signal(n, W_max, N):
     signals = [0] * N
@@ -23,7 +24,6 @@ def dft(signal):
             spectrum[n] += signal[k] * np.exp(-2j * np.pi * k * n / N)
     return spectrum
 
-
 def fft(signal):
     N = len(signal)
     spectrum = [0] * N
@@ -45,7 +45,29 @@ def fft(signal):
         root = root * inverted_root
     return spectrum
 
+def interval(start, stop, step):
+    signals = []
+    N = []
 
+    for i in range(start, stop + 1, step):
+        signals.append(gen_signal(n, W_max, i))
+        N.append(i)
+
+    return [N, signals] 
+
+def get_time(interval, ft):
+    time = []
+    N = interval[0]
+    signal = interval[1]
+
+    for i in range(0, len(N)):
+        start_time = t.time()
+        ft(signal[i])
+        time.append(t.time() - start_time)
+
+    return [N, time]
+    
+    
 n = int(input("Harmonics = "))
 W_max = int(input("Frequency = "))
 N = int(input("Number of points = "))
@@ -56,7 +78,12 @@ spectrum_dft = dft(signal)
 spectrum_fft = fft(signal)
 spectrum_numpy_fft = np.fft.fft(signal)
 
-plt.figure(figsize=(10, 7))
+signal_interval = interval(100, 1500, 100)
+time_dft = get_time(signal_interval, dft)
+time_fft = get_time(signal_interval, fft)
+
+
+plt.figure(1, figsize=(10, 7))
 
 plt.subplot(2, 2, 1)
 plt.title('Signal')
@@ -73,6 +100,23 @@ plt.plot(np.abs(spectrum_fft), 'k')
 plt.subplot(2, 2, 4)
 plt.title('Spectrum(numpy.fft)')
 plt.plot(np.abs(spectrum_numpy_fft), 'k')
+
+
+plt.figure(2, figsize=(10, 7))
+
+plt.subplot(3, 1, 1)
+plt.title('Time of dft')
+plt.plot(time_dft[0], time_dft[1], 'k')
+
+plt.subplot(3, 1, 2)
+plt.title('Time of fft')
+plt.plot(time_fft[0], time_fft[1], 'k')
+
+plt.subplot(3, 1, 3)
+plt.title('Comparison of dft and fft')
+plt.plot(time_dft[0], time_dft[1], label = "dft")
+plt.plot(time_fft[0], time_fft[1], label = "fft")
+plt.legend()
 
 plt.tight_layout()
 plt.show()
